62a5eddb9a4ffe963a9b16ddd8e7f08decb1ac38
andifirwansyah <andifirwansyah250@gmail.com>
2024-07-24 13:26:17 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>Halo</div>
+        <button>
+            <a href="penabur://forgot_password/jbf-cajxkd-91b7d52ceb61b90f97db">
+                <div>Go To App</div>
+            </a>
+        </button>
+    </div>
+</template>
+<script lang="ts" setup>
+onMounted(() => {
+    setTimeout(() => {
+        window.open('penabur://forgot_password/jbf-cajxkd-91b7d52ceb61b90f97db')
+    }, 1000)
+})
+</script>
